aac75c3772c8b551fc877d4f6c5253a894b6a27d
Biel Carpi <biel.carpi@students.salle.url.edu>
2022-02-08 09:11:38 +0100

    Implement User class

---
+++ src/User.java
@@ -0,0 +1,27 @@
+public class User {
+    private int id;
+    private String name;
+    private String alias;
+    private String[] interests;
+
+    public User(int id, String name, String alias, String[] interests){
+        this.id = id;
+        this.name = name;
+        this.alias = alias;
+        this.interests = interests;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAlias(){
+        return alias;
+    }
+    public String[] getInterests(){
+        return interests;
+    }
+
+}
