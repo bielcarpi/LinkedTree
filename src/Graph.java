47f80f04e777d9b67f2429f9f6d7d26e8daf9abf
Guillem <gody08112002@gmail.com>
2022-02-14 08:47:22 +0100

    Arrays afegits que plantejen el Graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,11 +1,17 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Graph {
+    private static User[] users;
+    private static Follow[] follows;
+
     public Graph(){
         try{
             String fileName = "graphXS.paed";
             ReadGraph.read(fileName);
+            users = ReadGraph.getUsers();
+            follows = ReadGraph.getFollows();
         }catch (IOException e){
             e.printStackTrace();
         }
