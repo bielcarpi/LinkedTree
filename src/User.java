d55bbf0b2fac407a65a31b699bc646a286d73155
Biel Carpi <biel.carpi@students.salle.url.edu>
2022-02-21 09:19:16 +0100

    Add comments to User class

--- src/User.java
+++ src/User.java
@@ -8,6 +8,12 @@ public class User implements Comparable<User>{
     private String[] interests;
     private ArrayList<Follow> follows;
 
+    /**
+     * userAux is an auxiliar variable for better performance
+     * on the method {@link #getUserWithId(int)}
+     * Instead of creating a user each time the method is called,
+     * we'll cache a user and change its ID
+     */
     private static User userAux;
 
     public User(int id, String name, String alias, String[] interests){
@@ -17,6 +23,14 @@ public class User implements Comparable<User>{
         this.interests = interests;
     }
 
+    /**
+     * Returns a User with the ID passed as parameter
+     * <p>To be used only for user comparison, as the user returned
+     * is not a valid user (all its attributes are null)
+     *
+     * @param id The ID of the user to be returned
+     * @return A user with the ID specified
+     */
     public static User getUserWithId(int id) {
         if(userAux == null) return new User(id, null, null, null);
 
@@ -48,6 +62,12 @@ public class User implements Comparable<User>{
     }
 
 
+    /**
+     * Compares the current user to another user, using its IDs
+     * @param that The user to be compared to
+     * @return A negative number, 0 or a positive number if this user's ID is
+     * less, equal or greater than the other user
+     */
     @Override
     public int compareTo(User that) {
         return this.id - that.id;
