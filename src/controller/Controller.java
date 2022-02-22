aa78270b132cbe57393c7ecb51fbe6dc85df0561
Biel Carpi <biel.carpi@students.salle.url.edu>
2022-02-22 09:11:49 +0100

    Add main menus

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -15,5 +15,27 @@ public class Controller {
 
     public void start(){
         view.printStartMenu();
+        int option;
+        do{
+            option = view.askForInteger();
+            switch (option){
+                case 1:
+                    // Execute Grafs Option
+                    char grafOption = view.askForChar();
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break:
+                case 5:
+                    break;
+                default:
+                    view.printMessage("Error. The value ranges are from [1, 5]");
+                    break;
+            }
+        }while(option < 1 || option > 6);
     }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,9 +1,20 @@
 package view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class View {
+
+    private static final String LINKED_TREE_LOGO = """
+              _      _       _            _   _______           \s
+             | |    (_)     | |          | | |__   __|          \s
+             | |     _ _ __ | | _____  __| |    | |_ __ ___  ___\s
+             | |    | | '_ \\| |/ / _ \\/ _` |    | | '__/ _ \\/ _ \\
+             | |____| | | | |   <  __/ (_| |    | | | |  __/  __/
+             |______|_|_| |_|_|\\_\\___|\\__,_|    |_|_|  \\___|\\___|
+                                                                \s
+            """;
     private static final String LINKED_TREE_MENU = """
-            .*LinkedTree*.
-            
             1. Seguidors (Grafs)
             2. A ESPECIFICAR
             3. A ESPECIFICAR
@@ -24,10 +35,44 @@ public class View {
             E. Tornar Enrere
             
             Quina funcionalitat vols executar?\s""";
-    public View(){
 
+    private Scanner scanner;
+
+        private static final String INTEGER_EXCEPTION_MESSAGE = """
+            Error, not an integer.
+            Please enter a valid integer:\s""";
+
+    public View() {
+        scanner = new Scanner(System.in);
     }
 
     public void printStartMenu(){
+        System.out.println(LINKED_TREE_LOGO);
+        System.out.print(LINKED_TREE_MENU);
+    }
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
+    public void printFollowersMenu() {
+        System.out.println(FOLLOWERS_MENU);
+    }
+
+    /**
+     * Metode que demana a l'usuari un enter controlant l'excepcio de Input Mismatch
+     * @return l'enter que ha escrit l'usuari
+     */
+    public int askForInteger() {
+        while (true) {
+            try {
+                return scanner.nextInt();
+
+            } catch (InputMismatchException e) {
+                System.out.println(INTEGER_EXCEPTION_MESSAGE);
+            } finally {
+                scanner.nextLine();
+            }
+        }
     }
 }
