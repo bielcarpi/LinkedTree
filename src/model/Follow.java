9462e736b39f91dc47bab3316adeb09bdf2b8284
Biel Carpi <biel.carpi@students.salle.url.edu>
2022-02-24 15:31:04 +0100

    Graph structure comment clarification

--- src/model/Follow.java
+++ src/model/Follow.java
@@ -1,6 +1,13 @@
 package model;
 
 public class Follow {
+    /**
+     * Note: For better performance, instead of saving the ID of the user followed
+     * we should save the reference to the User directly.
+     * Although our solution is not the best, we've optimized it and each time
+     * we need to find a user with its id we use binary search (as the graph is ordered
+     * using quickSort)
+     */
     private int idUserFollowed;
     private int timestamp;
     private int interactions;
