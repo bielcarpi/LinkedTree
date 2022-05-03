9aed3b2611e563e7a99a61ba3a089e3938c9efc0
Guillem <gody08112002@gmail.com>
2022-05-03 10:41:24 +0200

    Correcció en la cerca especial (criteri)

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
     private static final String FILE_GRAPH_NAME = "graphXL.paed";
     private static final String FILE_DRAMA_NAME = "dagXXL.paed";
     private static final String FILE_TREE_NAME = "treeS.paed";
-    private static final String FILE_R_TREE_NAME = "rtreeS.paed";
+    private static final String FILE_R_TREE_NAME = "rtreeL.paed";
     private static final int R_TREE_ORDER = 3;
 
     public static void main(String[] args){
