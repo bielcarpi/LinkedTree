da6519e8ec9dbe3dafd4217fef424bf2ff5b46ce
Biel Carpi <biel.carpi@students.salle.url.edu>
2022-05-10 08:21:29 +0200

    Fix bug comparing points

--- src/model/rtree/Rectangle.java
+++ src/model/rtree/Rectangle.java
@@ -118,9 +118,10 @@ public class Rectangle {
      */
     public void shrinkWithRectangles(ArrayList<Rectangle> rectangles){
         Point[] possiblePoints = new Point[rectangles.size() * 2];
-        for(int i = 0, k = 0; i < rectangles.size(); i++, k++){
-            possiblePoints[k] = rectangles.get(i).getP1();
-            possiblePoints[++k] = rectangles.get(i).getP2();
+        int i = 0;
+        for(Rectangle r: rectangles){
+            possiblePoints[i++] = r.getP1();
+            possiblePoints[i++] = r.getP2();
         }
 
         Point[] newRectangle = getRectangle(possiblePoints);
