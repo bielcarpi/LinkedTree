51a5c31ccefe94a3552e1c9110c7345990838d38
Biel Carpi <biel.carpi@students.salle.url.edu>
2022-05-11 09:55:59 +0200

    Add comments & Fix bug on HashMap class

--- src/model/table/HashMap.java
+++ src/model/table/HashMap.java
@@ -5,18 +5,37 @@ import model.utilities.Tuple;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class HashMap<K, T> {
+/**
+ * The class HashMap implements basic functionality of a Hash Table
+ * <p>Its internal space grows dynamically and ensures that there is always space to add new elements
+ * <p>It does not implement the {@link java.util.Map} interface, but it provides basic HashMap functionality,
+ * with methods such as {@link #put(Object, Object)}, {@link #remove(Object)} and {@link #get(Object)}
+ *
+ * @param <K> The type of key that will be used (take into account that the method {@code K.toString()} will
+ *           be used as a String is necessary for the key to be generated)
+ * @param <V> The type of object that will be saved
+ */
+public class HashMap<K, V> {
 
-    private ArrayList<Tuple<K, T>>[] internalArray; //Each position of the array has an arraylist
+    private ArrayList<Tuple<K, V>>[] internalArray; //Each position of the array has an arraylist
     private int filledPositions; //The positions of the array that have been filled with an arraylist
     private static final int DEFAULT_CAPACITY = 500;
 
+    /**
+     * Default Constructor.
+     * <p>It creates a new HashMap with the default capacity
+     */
     public HashMap(){
         internalArray = new ArrayList[DEFAULT_CAPACITY];
         filledPositions = 0;
     }
 
-    public void put(K key, T object){
+    /**
+     * Given a key and value, the method puts it into the hashmap for later retrieval
+     * @param key The key of the object
+     * @param object The object
+     */
+    public void put(K key, V object){
         int position = getHash(key);
         if(internalArray[position] == null){
             internalArray[position] = new ArrayList<>();
@@ -27,11 +46,16 @@ public class HashMap<K, T> {
         checkGrowing(); //Checks if the internal array needs to grow
     }
 
+    /**
+     * Given a key, if the object is found in the hashmap it is removed
+     * @param key The key of the object that wants to be deleted
+     * @return Whether the deletion has been successful (the object existed) or not
+     */
     public boolean remove(K key){
         int position = getHash(key);
         if(internalArray[position] == null) return false;
 
-        for(Tuple<K, T> entry: internalArray[position]){
+        for(Tuple<K, V> entry: internalArray[position]){
             if(entry.getFirst().equals(key)){
                 internalArray[position].remove(entry);
                 filledPositions--;
@@ -42,14 +66,33 @@ public class HashMap<K, T> {
         return false;
     }
 
-    public T get(K key){
+    /**
+     * Given a key, it returns the value for it on the hashmap
+     * @param key The key to retrieve the value
+     * @return The value for the given key
+     */
+    public V get(K key){
         int position = getHash(key);
         if(internalArray[position] == null) return null;
 
-        for(Tuple<K, T> entry: internalArray[position])
+        for(Tuple<K, V> entry: internalArray[position])
             if(entry.getFirst().equals(key)) return entry.getLast();
 
         return null;
+    }
+
+    /**
+     * Returns all the values currently contained in the hashmap within an ArrayList of V
+     * @return All the values currently contained in the hashmap
+     */
+    public ArrayList<V> getAllValues(){
+        ArrayList<V> values = new ArrayList<>();
+        for(ArrayList<Tuple<K, V>> entry: internalArray) {
+            if (entry != null)
+                for (Tuple<K, V> t : entry) values.add(t.getLast());
+        }
+
+        return values;
     }
 
 
